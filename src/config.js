1168b83d5f32820f44bd852844c96fc582a27872
Glitch (lc-bell-schedule) <none>
2022-08-22 12:42:10 +0000

    Squashed some BUGS!  Yay

--- src/config.js
+++ src/config.js
@@ -1,12 +1,13 @@
-import sky from "./schedules/sky.js";
-import half from "./schedules/half.js";
+import normal from "./schedules/normal.js";
+import peprally from "./schedules/peprally.js";
+import extended2nd from "./schedules/extended2nd.js"
 
 const config = {
     schedule: {
         //default schedule - schedule used most often. User will be
         //notified if today's schedule is not the default.
-        default: sky,
-        order: [null, sky, sky, half, sky, sky],
+        default: normal,
+        order: [null, normal, normal, normal, normal, normal],
         //alt schedule - if this is set, it will override the daily order.
         alt: null
     }
