6fcd71ceae3b55b080806c4a1d9a474d8e739121
Glitch (lc-bell-schedule) <none>
2023-01-03 16:46:05 +0000

    🍋🦆 Updated with Glitch

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ const config = {
         //default schedule - schedule used most often. User will be
         //notified if today's schedule is not the default.
         default: normal,
-        order: [null, normal, finals, finals, finals, oneFinals],
+        order: [null, normal, normal, normal, normal, normal],
         //alt schedule - if this is set, it will override the daily order.
         alt: null,
         showAnnoucements: false
