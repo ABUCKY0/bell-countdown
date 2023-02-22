a5bc294216da85b50ef746974ba130f2ec0fa74c
NotJohnnyTamale <81783950+ABUCKY0@users.noreply.github.com>
2023-02-22 00:22:35 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ const config = {
         default: normal,
         order: [null, normal, normal, normal, normal, normal],
         //alt schedule - if this is set, it will override the daily order.
-        alt: null,
+        alt: peprally,
         showAnnoucements: false
     }
 }
