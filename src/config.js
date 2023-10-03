f575e8bc83a9cd25501f8ef8d687ca00e98dd5b9
NotJohnnyTamale <81783950+ABUCKY0@users.noreply.github.com>
2023-10-03 04:40:33 +0000

    Setup PSAT

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 import normal from "./schedules/normal.js";
 import peprally from "./schedules/peprally.js";
 import pack from "./schedules/extended2nd.js";
+import psat from "./schedules/psat.js";
 //import finals from "./schedules/finalsTwoFinals.js";
 //import oneFinals from "./schedules/finalsWeekOnlyOne.js";
 //import unknown from "./schedules/unknown.js";
@@ -13,8 +14,7 @@ const config = {
         default: normal,
         order: [null, normal, normal, normal, normal, normal],
         //alt schedule - if this is set, it will override the daily order.
-        alt: null,
-        showAnnoucements: true
+        alt: psat
     },
     version: " v2.2.1"
 }
