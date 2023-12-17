ea910f26a8bf8d02c840c2b4a90d1a780df747e8
NotJohnnyTamale <81783950+ABUCKY0@users.noreply.github.com>
2023-12-16 21:36:45 -0600

    Update Config.js for finals week

--- src/config.js
+++ src/config.js
@@ -3,18 +3,18 @@ import peprally from "./schedules/peprally.js";
 import pack from "./schedules/extended2nd.js";
 
 //finals
-import mon from "./schedules/finals/monday.js";
-import tue from "./schedules/finals/tuesday.js";
-import wed from "./schedules/finals/wednesday.js";
-import thu from "./schedules/finals/z_thursday.js";
+import final_mon from "./schedules/finals/monday.js";
+import final_tue from "./schedules/finals/tuesday.js";
+import final_wed from "./schedules/finals/wednesday.js";
+import final_thu from "./schedules/finals/z_thursday.js";
 
 const config = {
     schedule: {
         //default schedule - schedule used most often. User will be
         //notified if .today's schedule is not the default.
         default: normal,
-        order: [null, normal, normal, normal, normal, normal],
-        //order: [null, mon, tue, wed, thu, null],
+        //order: [null, normal, normal, normal, normal, normal],
+        order: [null, final_mon, final_tue, final_wed, final_thu, null],
         //alt schedule - if this is set, it will override the daily order.
         alt: null
     },
