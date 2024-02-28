56d379ef96cf85d7bf22e18177f449f35fe6ac20
NotJohnnyTamale <81783950+ABUCKY0@users.noreply.github.com>
2024-02-28 10:04:02 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -13,7 +13,7 @@ const config = {
         //default schedule - schedule used most often. User will be
         //notified if .today's schedule is not the default.
         default: normal,
-        order: [null, normal, normal, peprally, normal, normal],
+        order: [null, normal, normal, pack, normal, normal],
         //order: [null, final_mon, final_tue, final_wed, final_thu, null],
         //alt schedule - if this is set, it will override the daily order.
         alt: null
